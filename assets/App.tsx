3df7c191f10db6220103b3e16a793bbca7188067
ekastn <118880703+ekastn@users.noreply.github.com>
2024-11-28 13:59:29 +0700

    feat: add solidjs

---
+++ assets/App.tsx
@@ -0,0 +1,11 @@
+import { Component } from "solid-js";
+
+const App: Component = () => {
+    return (
+        <div class="bg-slate-900 h-screen">
+            <h1 class="text-6xl text-white">hello solid</h1>
+        </div>
+    );
+};
+
+export default App;
--- assets/main.ts
+++
@@ -1,6 +0,0 @@
-import Experience from "./experience/Experience";
-
-const experienceContainer = document.querySelector("#experience-container") as HTMLElement;
-const experience = new Experience(experienceContainer);
-
-experience.start();
