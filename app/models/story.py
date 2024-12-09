c9e144d00964bd89ec967648bf751bc157738469
Octa Toriq <octa.toriq@gmail.com>
2024-12-09 18:44:29 +0700

    feat add pins and story model

---
+++ app/models/story.py
@@ -0,0 +1,24 @@
+from flask_login import UserMixin
+from sqlalchemy import func, types
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm.base import Mapped
+from sqlalchemy.sql.sqltypes import String
+from typing_extensions import Optional
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from app import db
+
+class Story(db.Model, UserMixin):
+    __tablename__ = "stories"
+
+    id: Mapped[int] = mapped_column("story_id", primary_key=True)
+    tittle: Mapped[str] = mapped_column(String(128), unique=False, nullable=False)
+    content: Mapped[str] = mapped_column(String(1024), unique=False, nullable=False)
+    created_at: Mapped[types.DateTime] = mapped_column(types.DateTime(255), nullable=True,default=func.current_timestamp())
+    updated_at: Mapped[types.DateTime] = mapped_column(types.DateTime(100), nullable=True,default=func.current_timestamp())
+
+    def __repr__(self):
+        return f"<Story: {self.tittle}>"
+    
+
+
